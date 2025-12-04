31542d657d44e4c7daaa2f9752fb6fb13fb2cafd
KhangSimple <you@example.com>
2025-12-05 02:51:18 +0700

    fix

--- dags/const/business_query_const/query_const.py
+++ dags/const/business_query_const/query_const.py
@@ -5,20 +5,35 @@ REVENUE_BY_DATE_QUERY = """
     using (product_sk)
     join `hackathon-478514.mart_ds.dm_fact_order` C
     using (order_sk)
-    WHERE C.status = 'delivered'
+    WHERE C.status = 'Delivered'
     GROUP BY date
 """
 
 REVENUE_QUERY = """
     CREATE OR REPLACE TABLE `hackathon-478514.sale_ds.revenue` AS
-    SELECT A.updated_at as date,
-    EXTRACT(DATE FROM A.updated_at) as day,
-    EXTRACT(MONTH FROM A.updated_at) as month,
-    EXTRACT(YEAR FROM A.updated_at) as year,
+    SELECT C.updated_at as date,
+    EXTRACT(DATE FROM C.updated_at) as day,
+    EXTRACT(MONTH FROM C.updated_at) as month,
+    EXTRACT(YEAR FROM C.updated_at) as year,
     total_price as total_price FROM `hackathon-478514.mart_ds.dm_fact_order_item` A
     join `hackathon-478514.mart_ds.dm_dim_product` B
     using (product_sk)
     join `hackathon-478514.mart_ds.dm_fact_order` C
     using (order_sk)
-    WHERE C.status = 'delivered';
+    WHERE C.status = 'Delivered';
+"""
+
+REVENUE_BY_CATEGORY_QUERY = """
+    CREATE OR REPLACE TABLE `hackathon-478514.sale_ds.revenue_by_category` AS
+    SELECT
+    C.updated_at as date,
+    IFNULL(D.name, 'Not categoried') as category_name,
+    total_price as total_price
+    FROM `hackathon-478514.mart_ds.dm_fact_order_item` A
+    join `hackathon-478514.mart_ds.dm_dim_product` B
+    using (product_sk)
+    join `hackathon-478514.mart_ds.dm_fact_order` C
+    using (order_sk)
+    left join `hackathon-478514.mart_ds.dm_dim_category` D on B.category_id = D.category_id
+    WHERE C.status = 'Delivered';
 """
